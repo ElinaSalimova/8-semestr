d980357167910839ae9fe8cbafb567de0cc44a8a
ElinaSalimova <36744218+ElinaSalimova@users.noreply.github.com>
2019-02-27 13:45:53 +0300

    Add files via upload

---
+++ laba1.R
@@ -0,0 +1,98 @@
+# рейтинг телевизоров с Я.маркета
+
+library('rvest') 
+# URL страницы для скраппинга
+url <- 'https://market.yandex.ru/catalog--televizory/59601/list?hid=90639&suggest_text=%D0%A2%D0%B5%D0%BB%D0%B5%D0%B2%D0%B8%D0%B7%D0%BE%D1%80%D1%8B&suggest=1&suggest_type=category&onstock=1&local-offers-first=0'
+
+# читаем HTML страницы
+webpage <- read_html(url) 
+
+# функция перебора тегов внутри тегов более высокого уровня
+get_tags <- function(node){
+  # найти все теги с метарейтингом
+  raw_data <- html_nodes(node, selector) %>% html_text 
+  # значения нулевой длины (для фильма нет такого тега) меняем на пропуски
+  data_NAs <- ifelse(length(raw_data) == 0, NA, raw_data) }
+
+# Название телевизоров ----
+name_data <- html_nodes(webpage, '.n-snippet-card2__title') %>% html_text 
+length(name_data)
+head(name_data)
+
+# Описание 
+# Описание1
+description1_data <- html_nodes(webpage,'div.n-snippet-card2__content > ul> li:nth-child(1)') %>% html_text
+length(description1_data)
+head(description1_data)
+# Описание2
+description2_data <- html_nodes(webpage,'div.n-snippet-card2__content > ul> li:nth-child(2)') %>% html_text
+length(description2_data)
+head(description2_data)
+# Описание3
+description3_data <- html_nodes(webpage,'div.n-snippet-card2__content > ul> li:nth-child(3)') %>% html_text
+length(description3_data)
+head(description3_data)
+
+# Цена на телевизор ----
+price_data <- html_nodes(webpage, 'div.n-snippet-card2__price > div > div > a > div')%>% html_text
+price_data <- gsub( '\\W','', price_data)
+length(price_data)
+head(price_data)
+price_data <- as.numeric(price_data)
+
+# Оценка ----
+selector <- 'div.rating__value'
+doc <- html_nodes(webpage, 'div.n-snippet-card2__header-stickers')
+stars_data <-sapply(doc, get_tags)
+length(stars_data)
+head(stars_data)
+stars_data <- as.numeric(stars_data)
+
+# Количество отзывов ----
+selector <- 'span'
+doc <- html_nodes(webpage, 'div.n-snippet-card2__header-stickers')
+counto_data <- sapply(doc, get_tags)
+length(counto_data)
+head(counto_data)
+counto_data <- gsub( '\\W','', counto_data)
+counto_data <- as.numeric(counto_data)
+
+# Количество предложений ----
+countp_data <- html_nodes(webpage, 'div.n-snippet-card2__more-prices-link > a') %>%html_text
+length(countp_data)
+head(countp_data)
+countp_data <- gsub( '\\W','', countp_data)
+countp_data <- as.numeric(countp_data)
+
+
+
+# Количество человек, купивших товар ----
+selector <- 'span'
+doc <- html_nodes(webpage, 'div.n-snippet-card2__content')
+countb_data <- sapply(doc, get_tags)
+length(countb_data)
+head(countb_data)
+countb_data <- gsub( '\\W','', countb_data)
+countb_data <- as.numeric(countb_data)
+
+# Последний отзыв ----
+selector <- 'div.n-badge-review__text > a'
+doc <- html_nodes(webpage, 'div.n-snippet-card2__bottom')
+lasto_data <- sapply(doc, get_tags)
+length(lasto_data)
+head(lasto_data)
+
+# совмещаем данные в один фрейм
+DF_TV <- data.frame(Name = name_data, Description1 = description1_data,
+                          Description2 = description2_data, Description3 = description3_data,
+                          Price = price_data, Stars = stars_data, Counto = counto_data,
+                          Countp = countp_data, 
+                          Lasto = lasto_data,
+                          stringsAsFactors = F)
+# результат
+dim(DF_TV)
+str(DF_TV)
+
+# записываем в .csv
+write.csv(DF_TV, file = 'TV_Ya.Market.csv', row.names = F)
+
